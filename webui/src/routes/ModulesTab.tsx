1d39cd86f150da05688cad27393fcbde254cca60
LegacyFreeman <legacy.freeman.world@gmail.com>
2026-01-17 22:29:07 +0200

    fix

--- webui/src/routes/ModulesTab.tsx
+++ webui/src/routes/ModulesTab.tsx
@@ -173,7 +173,6 @@ export default function ModulesTab() {
                             </span>
                           </div>
                         </Show>
-                      </div>
                     </Show>
                   </div>
                 )}
